e1548c88f24d14f93c6ac9ea057644cecf5aeede
Christianmj <christianmj94@hotmail.dk>
2022-11-07 12:29:33 +0100

    public mapper tilføjet

--- src/controller.js
+++ src/controller.js
@@ -4,13 +4,17 @@ module.exports = (req, res) => {
 
     const incoming = new URL(req.url, "http://localhost:3003");
 
-    const endpoint = incoming.pathname;    
-
-    //console.log(endpoint);
+    const endpoint = incoming.pathname;
 
     //jex.im for rout diagram
     //regex101 for test
     const regex = /^\/(html|css|img|js)\/\w+\.(html|js|css|png|jpe?g|gif|tiff|bmp)$/;
+    const match = endpoint.match(regex);
+    if (match) {
+        utils.sendFile(res, match[0]);
+    }
+
+    console.log(match);
 
     //utils.sendText(res, "Hilsen fra serveren");
     utils.sendJson(res, { "message": "Okay...."});
